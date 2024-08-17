89f67fef41971d96a64c2d0c796361d4f59814c8
fesper-s <fesper-s@student.42.rio>
2024-08-17 12:14:35 -0300

    updated: tarefa-5

--- tarefa-5/mergeArrays.ts
+++ tarefa-5/mergeArrays.ts
@@ -1,3 +1,20 @@
-export function mergeArrays(array1: {}[], array2: {}[]): {}[] {
-    
+type ArrayType = {
+    id: number,
+    [key: string]: any
+}
+
+export function mergeArrays(array1: ArrayType[], array2: ArrayType[]): ArrayType[] {
+    const result: ArrayType[] = [...array1];
+
+    array2.forEach((item2) => {
+        const index: number = result.findIndex(item1 => item1.id === item2.id);
+        if (index !== -1) {
+            result[index] = { ...result[index], ...item2 };
+        }
+        else {
+            result.push(item2);
+        }
+    });
+
+    return result;
 }
